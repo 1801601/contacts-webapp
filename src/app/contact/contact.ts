00f0579dd2aa871675ac9e60039fd19e62b83f1b
1801061 <1801061@co.saimia.fi>
2018-05-16 14:14:15 +0300

    Laajennettu henkilötietoja

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -2,11 +2,21 @@ export class Contact {
   id: number;
   firstName: string;
   lastName: string;
+  streetAddress: string;
+  city: string;
+  zipCode: string;
+  phoneNumber: string;
+  email: string;
 
 
-  constructor(id?: number, firstName?: string, lastName?: string) {
+  constructor(id?: number, firstName?: string, lastName?: string, streetAddress?: string, city?: string, zipCode?: string, phoneNumber?: string, email?: string) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
+    this.streetAddress = streetAddress;
+    this.city = city;
+    this.zipCode = zipCode;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
   }
 }
