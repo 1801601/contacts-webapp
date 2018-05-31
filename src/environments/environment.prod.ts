1a0a0c2327075f05c7893956d0bec43d378b5cc7
1801061 <1801061@co.saimia.fi>
2018-05-31 14:41:25 +0300

    Loginia

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiEndpointUrl: 'http://localhost:53066/api/contacts'
+  apiEndpointUrl: 'http://esancontactswebapi.azurewebsites.net'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiEndpointUrl: 'http://localhost:53066/api/contacts'
+  apiEndpointUrl: 'http://esancontactswebapi.azurewebsites.net'
 };
 
 /*
